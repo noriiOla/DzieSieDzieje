14104f586b1144de3c26d63b8754740279612f58
Wolodja <wolodjawaskovych@i.ua>
2019-03-17 20:34:37 +0100

    Project setup

---
+++ src/main/java/com/dziesiedzieje/dziesiedzieje/DzieSieDziejeApplication.java
@@ -0,0 +1,13 @@
+package com.dziesiedzieje.dziesiedzieje;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DzieSieDziejeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DzieSieDziejeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dziesiedzieje/dziesiedzieje/DzieSieDziejeApplicationTests.java
@@ -0,0 +1,16 @@
+package com.dziesiedzieje.dziesiedzieje;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DzieSieDziejeApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
